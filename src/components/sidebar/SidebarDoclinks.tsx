1cb57d3177ca12412aeb51908a131119828e01b9
Paul Everitt <pauleveritt@me.com>
2019-02-27 09:41:53 -0500

    Use the shorter notation.

--- src/components/sidebar/SidebarDoclinks.tsx
+++ src/components/sidebar/SidebarDoclinks.tsx
@@ -11,7 +11,7 @@ export interface ISidebarDoclinksProps {
   links: IDoclink[];
 }
 
-const Doclink: React.FunctionComponent<IDoclink> = ({ label, target }) => (
+const Doclink: React.FC<IDoclink> = ({ label, target }) => (
   <li>
     <ScrollLink activeClass="active" to={target} spy={true} smooth={true} offset={0} duration={500} style={{ width: 'auto' }}>
       {label}
@@ -19,7 +19,7 @@ const Doclink: React.FunctionComponent<IDoclink> = ({ label, target }) => (
   </li>
 );
 
-const SidebarDoclinks: React.FunctionComponent<ISidebarDoclinksProps> = ({ links }) => {
+const SidebarDoclinks: React.FC<ISidebarDoclinksProps> = ({ links }) => {
   return (
     <div className="bio-page-sidebar-references-group" style={{ marginTop: '1rem' }}>
       {links.length > 0 && (
