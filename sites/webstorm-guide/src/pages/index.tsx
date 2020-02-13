501de5c2f8d5cea502554bc524add6bdd820e544
Paul Everitt <pauleveritt@me.com>
2020-02-13 11:53:12 -0500

    Update text on home page.

--- sites/webstorm-guide/src/pages/index.tsx
+++ sites/webstorm-guide/src/pages/index.tsx
@@ -20,7 +20,7 @@ const IndexPage: React.FC = () => {
                   <div className="column is-two-thirds">
 
                     <p className="is-size-4">Find out how to be more productive and write high-quality code faster with
-                      a series of tips and tricks, carefully selected for you in one place by the WebStorm team.</p>
+                      a series of tips and tricks, carefully selected for you in one place.</p>
                   </div>
                 </div>
               </div>
@@ -33,10 +33,15 @@ const IndexPage: React.FC = () => {
               <h1 className="title">Learn something new quickly</h1>
               <div className="columns">
                 <div className="column is-four-fifths-desktop">
-                  To start learning, simply jump to the section you find most relevant available within two broader categories:
+                  To start learning, simply jump to the section you find most relevant available within two broader
+                  categories:
                   <ul>
-                    <li><strong>Technologies</strong>: learning resources organized by languages, libraries, and frameworks.</li>
-                    <li><strong>Topics</strong>: tips and tricks organized by a specific topic, e.g. debugging or code editing.</li>
+                    <li><strong>Technologies</strong>: go through some handy tips and tricks organized by languages,
+                      libraries, and frameworks.
+                    </li>
+                    <li><strong>Topics</strong>: explore a collection of tips organized by a specific topic, such as
+                      debugging or code editing.
+                    </li>
                   </ul>
                   <p>If you’ve got an idea how to make this guide better or want to share your feedback with us, go to
                     the About page to find out how you can do that.</p>
