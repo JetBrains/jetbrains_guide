81a008607fe7f23fc9e312f3ecfedf65a8823e73
Paul Everitt <pauleveritt@me.com>
2020-02-14 14:16:13 -0500

    Change the description wording for each listing page.

--- packages/gatsby-theme-bulmaio/src/resourcetypes/tutorial/Tutorials.tsx
+++ packages/gatsby-theme-bulmaio/src/resourcetypes/tutorial/Tutorials.tsx
@@ -1,28 +1,29 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
+import {graphql, useStaticQuery} from 'gatsby';
 import ReferenceLayout from 'gatsby-theme-bulmaio/src/components/layout/ReferenceLayout';
-import { ListedResourcesQuery } from 'gatsby-theme-bulmaio/src/resourcetypes';
+import {ListedResourcesQuery} from 'gatsby-theme-bulmaio/src/resourcetypes';
 import makeResources from 'gatsby-theme-bulmaio/src/resourcetypes/makeResource';
 
 export const Tutorials: React.FC = () => {
-  const { resources }: ListedResourcesQuery = useStaticQuery(
-    graphql`
+    const {resources}: ListedResourcesQuery = useStaticQuery(
+        graphql`
 {
   resources: allResourcesByType(resourceType: "tutorial" ) {
     ...ListedResourceFragment
   }
 }
 `
-  );
-  return (
-    <ReferenceLayout pageTitle="Tutorials" subtitle="Learn specific technologies with multi-step lessons using code, text, and videos.">
-      {{
-        listing: (
-          <div>
-            {makeResources(resources)}
-          </div>
-        )
-      }}
-    </ReferenceLayout>
-  );
+    );
+    return (
+        <ReferenceLayout pageTitle="Tutorials"
+                         subtitle="Learn how to work with technologies using detailed, multi-step lessons accompanied by code samples, instructions, and videos.">
+            {{
+                listing: (
+                    <div>
+                        {makeResources(resources)}
+                    </div>
+                )
+            }}
+        </ReferenceLayout>
+    );
 };
