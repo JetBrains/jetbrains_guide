d7f532b1c081aac3ade79196829d931f7cc76a4a
Paul Everitt <pauleveritt@me.com>
2020-05-19 14:59:08 -0400

    Bring back the dotnet navbar customizations.

---
+++ sites/dotnet-guide/src/gatsby-theme-bulmaio/components/navbar/NavbarLink.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+
+export interface NavbarLinkProps {
+  color: string;
+  href: string;
+  icon: string;
+}
+
+const NavbarLink: React.FC<NavbarLinkProps> = ({ href, color, icon }) => {
+  if (icon === 'github')
+    return (<span>Twitter&nbsp;</span>);
+  let myIcon = 'fab fa-lg fa-' + icon;
+  let myStyle = {};
+  if (icon.startsWith('custom-')) {
+    myIcon = icon.substring('custom-'.length);
+    myStyle = {
+      width: '100%',
+      height: '100%',
+      'background-size': 'cover'
+    };
+  }
+  return (
+    <a data-testid={`navbarlink-a`} className="navbar-item is-hidden-touch" href={href} target="_blank">
+      <span data-testid={`navbarlink-span`} className="icon" style={{ color: `#${color}` }}>
+        <i data-testid={`navbarlink-i`} className={`${myIcon}`} style={myStyle} />
+      </span>
+    </a>
+  );
+};
+
+export default NavbarLink;
