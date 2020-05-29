d4d41edeb71a9d702d6b7462cdfd229917bb8401
Paul Everitt <pauleveritt@me.com>
2020-05-29 12:52:32 -0400

    Use the posix flavor of resolver to get slugs with forward slashes.

--- packages/gatsby-theme-bulmaio/src/resources/tutorialstep/createResolvers.ts
+++ packages/gatsby-theme-bulmaio/src/resources/tutorialstep/createResolvers.ts
@@ -1,4 +1,4 @@
-import { resolve } from 'path';
+import { posix } from 'path';
 
 export const tutorialStepCreateResolvers = async (createResolvers: any) => {
   // noinspection JSUnusedLocalSymbols
@@ -8,7 +8,7 @@ export const tutorialStepCreateResolvers = async (createResolvers: any) => {
         type: 'Tutorial',
         async resolve(source: any, args: any, context: any, info: any) {
           const thisSlug: string = source.slug;
-          const parentSlug = `${resolve(thisSlug, '..')}/`;
+          const parentSlug = `${posix.resolve(thisSlug, '..')}/`;
           // noinspection UnnecessaryLocalVariableJS
           const parentNode = context.nodeModel.getAllNodes({ type: `Tutorial` })
             .find((node: any) => node.slug === parentSlug);
@@ -18,4 +18,3 @@ export const tutorialStepCreateResolvers = async (createResolvers: any) => {
     }
   });
 };
-
