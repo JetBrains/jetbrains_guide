90471d663abb86ffc021adb79e334f2089ba08d6
Paul Everitt <pauleveritt@me.com>
2020-07-02 15:10:42 -0400

    Split the NavbarSearch into its own component.

--- packages/gatsby-theme-bulmaio/src/components/navbar/NavbarEnd.tsx
+++ packages/gatsby-theme-bulmaio/src/components/navbar/NavbarEnd.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import NavbarButton, { NavbarButtonProps } from './NavbarButton';
 import NavbarLink, { NavbarLinkProps } from './NavbarLink';
-import {Search} from "../search/Search";
-import {useState} from "react";
+import NavbarSearch from './NavbarSearch';
 
 export interface NavbarEndProps {
   buttons: NavbarButtonProps[];
@@ -10,30 +9,16 @@ export interface NavbarEndProps {
 }
 
 const NavbarEnd: React.FC<NavbarEndProps> = ({ links, buttons }) => {
-    const [isSearchVisible, setSearchVisible] = useState(false);
 
-    return (
-      <div className="navbar-end">
-        {links && links.map(link => <NavbarLink key={link.href} {...link} />)}
-        {buttons && buttons.filter(button => button.label != 'Skip').map(button => <NavbarButton key={button.href} {...button} />)}
+  return (
+    <div className="navbar-end">
+      {links && links.map(link => <NavbarLink key={link.href} {...link} />)}
+      {buttons && buttons.filter(button => button.label != 'Skip').map(button => <NavbarButton
+        key={button.href} {...button} />)}
 
-        <div className={"dropdown is-right" + (!isSearchVisible ? "" : " is-active")}>
-          <div className="dropdown-trigger">
-            <button className="button is-black" aria-haspopup="true" aria-controls="search-menu"
-                    onClick={() => setSearchVisible(!isSearchVisible)}>
-                      <span className="icon">
-                        <i className="fas fa-search" aria-hidden="true" />
-                      </span>
-            </button>
-          </div>
-          <div className="dropdown-menu" id="search-menu" role="menu">
-            <div className="dropdown-content">
-              <Search limit={10} />
-            </div>
-          </div>
-        </div>
-      </div>
-    )
+      <NavbarSearch />
+    </div>
+  );
 };
 
 export default NavbarEnd;
---
+++ packages/gatsby-theme-bulmaio/src/components/navbar/NavbarSearch.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { Search } from '../search/Search';
+
+export interface NavbarSearchProps {
+}
+
+const NavbarSearch: React.FC<NavbarSearchProps> = () => {
+  const [isSearchVisible, setSearchVisible] = useState(false);
+
+  // noinspection JSUnusedLocalSymbols
+  // const orig = (
+  //   <div className="navbar-end">
+  //     {links && links.map(link => <NavbarLink key={link.href} {...link} />)}
+  //     {buttons && buttons.filter(button => button.label != 'Skip').map(button => <NavbarButton
+  //       key={button.href} {...button} />)}
+  //   </div>
+  // );
+
+  return (
+    <div className="navbar-item">
+      <div className="control">
+        <div className={'dropdown is-right' + (!isSearchVisible ? '' : ' is-active')}>
+          <div className="dropdown-trigger">
+            <button className="button is-black" aria-haspopup="true" aria-controls="search-menu"
+                    onClick={() => setSearchVisible(!isSearchVisible)}>
+              <span className="icon">
+                <i className="fas fa-search" aria-hidden="true" />
+              </span>
+            </button>
+          </div>
+          <div className="dropdown-menu" id="search-menu" role="menu">
+            <div className="dropdown-content">
+              <Search limit={10} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarSearch;
