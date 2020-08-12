693c61e3483d629c1ed796fd671650c9b392eec3
Paul Everitt <pauleveritt@me.com>
2020-08-12 11:54:04 -0400

    Get the listing pages to, on small screens, have some padding. Fixes #116

--- packages/gatsby-theme-bulmaio/src/components/layout/ReferenceLayout.tsx
+++ packages/gatsby-theme-bulmaio/src/components/layout/ReferenceLayout.tsx
@@ -6,60 +6,64 @@ A variation of the MasterLayout which makes lists entries.
 
 import React from 'react';
 import SiteLayout from './SiteLayout';
-import { MDXRenderer } from 'gatsby-plugin-mdx';
+import {MDXRenderer} from 'gatsby-plugin-mdx';
 
 interface ReferenceLayoutProps {
-  pageTitle: string;
-  subtitle?: string;
-  bodyHtml?: string;
-  children: {
-    figure?: React.ReactNode;
-    listing: React.ReactNode;
-  };
+    pageTitle: string;
+    subtitle?: string;
+    bodyHtml?: string;
+    children: {
+        figure?: React.ReactNode;
+        listing: React.ReactNode;
+    };
 }
 
 const ReferenceLayout: React.FC<ReferenceLayoutProps> = (
-  {
-    pageTitle,
-    subtitle,
-    bodyHtml,
-    children: { figure, listing }
+    {
+        pageTitle,
+        subtitle,
+        bodyHtml,
+        children: {figure, listing}
 
-  }) => {
-  return (
-    <SiteLayout pageTitle={pageTitle}>
-      <main className="bd-main bulmaio-body">
-        <div className="bd-main-container container">
-          <header className="bd-header">
-            <article className="media">
-              {figure && <figure className="media-left">{figure}</figure>}
-              <div className="media-content">
-                <div className="content">
-                  <div className="bd-header-titles">
-                    <h1 className="title">{pageTitle}</h1>
-                    {subtitle && <p className="subtitle is-4">{subtitle}</p>}
-                  </div>
-                </div>
-              </div>
-            </article>
-          </header>
+    }) => {
+    return (
+        <SiteLayout pageTitle={pageTitle}>
+            <main className="bd-main bulmaio-body">
+                <div className="bd-main-container container">
+                    <div className="bd-duo">
+                        <div className="bd-lead">
+                            <header className="bd-header">
+                                <article className="media">
+                                    {figure && <figure className="media-left">{figure}</figure>}
+                                    <div className="media-content">
+                                        <div className="content">
+                                            <div className="bd-header-titles">
+                                                <h1 className="title">{pageTitle}</h1>
+                                                {subtitle && <p className="subtitle is-4">{subtitle}</p>}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </article>
+                            </header>
 
-          {bodyHtml && (
-            <div className="columns">
-              <div className="column is-three-quarters-desktop">
-                <div className="bd-content content">
-                  <MDXRenderer>{bodyHtml}</MDXRenderer>
+                            {bodyHtml && (
+                                <div className="columns">
+                                    <div className="column is-three-quarters-desktop">
+                                        <div className="bd-content content">
+                                            <MDXRenderer>{bodyHtml}</MDXRenderer>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                            <div className="columns">
+                                <div className="column is-three-quarters-desktop bio-resourcecards">{listing}</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          )}
-          <div className="columns">
-            <div className="column is-three-quarters-desktop bio-resourcecards">{listing}</div>
-          </div>
-        </div>
-      </main>
-    </SiteLayout>
-  );
+            </main>
+        </SiteLayout>
+    );
 };
 
 export default ReferenceLayout;
