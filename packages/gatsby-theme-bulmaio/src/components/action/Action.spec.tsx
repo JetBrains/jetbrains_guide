aafc0e0610a2e8de5d83bbe3ff47185cf29c03bf
Paul Everitt <pauleveritt@me.com>
2020-08-13 10:29:34 -0400

    Put the prototype of getOS in place.

--- packages/gatsby-theme-bulmaio/src/components/action/Action.spec.tsx
+++ packages/gatsby-theme-bulmaio/src/components/action/Action.spec.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import Action, {getOS} from './Action';
+import { Action, getOS } from './Action';
 
-test('Action', () => {
-    const { getByText } = render(<Action />);
-    expect(getByText('Linux')).toBeTruthy();
+test('Action Short Form', () => {
+  const { getByText } = render(<Action id="FindInPath" />);
+  const node = getByText('Cmd+Shift+F');
+  expect(node).toBeTruthy();
+  expect(node.getAttribute('title')).toEqual('Find In Path');
+});
+
+test('Action Long Form', () => {
+  const { getByText } = render(<Action id="FindInPath" longForm={true} />);
+  const node = getByText('Find In Path (Cmd+Shift+F)');
+  expect(node).toBeTruthy();
+  expect(node.getAttribute('title')).toBeNull();
 });
 
 test('getOS', () => {
-   const result = getOS();
-   expect(result).toEqual("Linux")
+  const result = getOS();
+  expect(result).toEqual('mac');
 });
--- packages/gatsby-theme-bulmaio/src/components/action/Action.tsx
+++ packages/gatsby-theme-bulmaio/src/components/action/Action.tsx
@@ -1,31 +1,108 @@
-import React from 'react';
+import React, { Component } from 'react';
+// ActionDatabase:
+// This will be a JSON file, written by some code that
+// converts the help-sources actions XML.
+// const ActionsDatabase: ActionEntries = import('./actions.json')
+import ActionDatabase from './actions.json';
 
-export const getOS = () => {
-    //TODO: use a better detection library for production
-    if(typeof window !== 'undefined') {
-        return "linux"
-    }
-
-    const osList = ['windows', 'linux', 'x11', 'mac'];
-    let  myOS = osList.find(v => window.navigator.appVersion.toLowerCase().indexOf(v) >= 0);
-    if (myOS === 'x11') {
-        myOS = 'linux'
-    }
-    return myOS !== undefined ? myOS : "florin"
+export function getOS() {
+  if (typeof window === 'undefined' && window['navigator']) {
+    // No window means we are running in Gatsby, not
+    // in the browser. Return a default. We also put the
+    // window['navigator'] test in there to test from Jest,
+    // which has window but not navigator.
+    return 'winlin';
+  }
+  // Otherwise, sniff the userAgent and return the platform
+  return 'mac';
 }
 
-const Action = () => {
-    const currentOs = getOS();
-
-    return (
-        <div>
-            <span>funcCall: {getOS()}</span>
-            <div>
-                {currentOs === 'linux' && <span>Linux</span>}
-                {currentOs === 'windows' && <span>Windows</span>}
-            </div>
-        </div>
-    )
+export interface ActionEntry {
+  description: string;
+  winlin: string;
+  mac: string;
 }
 
-export default Action;
+export interface ActionEntries {
+  [key: string]: ActionEntry
+}
+
+const ActionData: ActionEntries = ActionDatabase;
+
+export interface ActionProps {
+  id: string
+  longForm?: boolean
+}
+
+export interface ActionState {
+  os: string;
+  action: any;
+}
+
+export class Action extends Component<ActionProps, ActionState> {
+  static defaultProps = {
+    longForm: false
+  };
+
+  constructor(props: ActionProps) {
+    super(props);
+
+    // Since this gets run server-side, this is the place to
+    // look in the local file to get all the data for this
+    // one action. All of this data gets serialized into the
+    // component that is sent to the browser. Meaning, it has
+    // the original ActionEntry inside the action.
+    this.state = {
+      os: 'winlin',
+      action: ActionData[this.props.id]
+    };
+
+  }
+
+  componentDidMount() {
+    // This runs in the browser during what's called "component
+    // rehydration".
+    const os = getOS();
+
+    this.setState(
+      {
+        os,
+        ...this.state.action
+      }
+    );
+  }
+
+  render() {
+    /*
+
+    Provide two forms. You can say in Markdown:
+      Press <Action id="FindInPath"/> to find...
+
+    ...and get:
+      <span class="tag" title="Find In Path">Ctrl+Shift+F</span>
+
+    Or, the long form shows the action description as part of the
+    span:
+      Use <Action id="FindInPath"/>
+
+    ...gives:
+      <span class="tag">Find In Path (Ctrl+Shift+F)</span>
+
+     */
+    const longForm = this.props.longForm;
+    const description = this.state.action.description;
+    const shortcut = this.state.action[this.state.os];
+    if (longForm) {
+      return (
+        <span className="tag guide-action">{description} ({shortcut})</span>
+      );
+    } else {
+      return (
+        <span
+          className="tag guide-action"
+          title={description}
+        >{shortcut}</span>
+      );
+    }
+  }
+}
