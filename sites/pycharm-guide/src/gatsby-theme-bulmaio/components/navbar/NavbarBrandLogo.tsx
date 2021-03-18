35a234ee447b6305de8aa7d81ac9eacca349d18a
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2021-03-18 15:30:41 -0400

    Fix JetBrains Logo issues, need to import them now

--- sites/pycharm-guide/src/gatsby-theme-bulmaio/components/navbar/NavbarBrandLogo.tsx
+++ sites/pycharm-guide/src/gatsby-theme-bulmaio/components/navbar/NavbarBrandLogo.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { Link } from 'gatsby';
-import logo from './jetbrains-simple.svg'
+import logo from './jetbrains-simple.svg';
 
 const NavbarBrandLogo: FC = () => (
   <>
