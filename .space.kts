19429c871a092e23f7b707664c386ef166191d26
Maarten Balliauw <maarten.balliauw@jetbrains.com>
2021-03-26 17:07:47 +0000

    Add Space automation

---
+++ .space.kts
@@ -0,0 +1,167 @@
+job("Build PyCharm Guide") {
+    startOn {
+        gitPush {
+            pathFilter {
+                +"sites/pycharm-guide/**"
+                +"packages/**"
+                +"package.json"
+            }
+        }
+    }
+    
+    container("node:12") {
+        resources {
+            cpu = 2.cpu
+            memory = 4.gb
+        }
+        
+        shellScript {
+            content = """
+                npm install --global gatsby-cli yarn
+                yarn install
+                gatsby telemetry --disable
+                yarn run pc:build
+            """
+        }
+    }
+}
+
+job("Build GoLand Guide") {
+    startOn {
+        gitPush {
+            pathFilter {
+                +"sites/goland-guide/**"
+                +"packages/**"
+                +"package.json"
+            }
+        }
+    }
+    
+    container("node:12") {
+        resources {
+            cpu = 2.cpu
+            memory = 4.gb
+        }
+        
+        shellScript {
+            content = """
+                npm install --global gatsby-cli yarn
+                yarn install
+                gatsby telemetry --disable
+                yarn run go:build
+            """
+        }
+    }
+}
+
+job("Build WebStorm Guide") {
+    startOn {
+        gitPush {
+            pathFilter {
+                +"sites/webstorm-guide/**"
+                +"packages/**"
+                +"package.json"
+            }
+        }
+    }
+    
+    container("node:12") {
+        resources {
+            cpu = 2.cpu
+            memory = 4.gb
+        }
+        
+        shellScript {
+            content = """
+                npm install --global gatsby-cli yarn
+                yarn install
+                gatsby telemetry --disable
+                yarn run ws:build
+            """
+        }
+    }
+}
+
+job("Build Space Guide") {
+    startOn {
+        gitPush {
+            pathFilter {
+                +"sites/space-guide/**"
+                +"packages/**"
+                +"package.json"
+            }
+        }
+    }
+    
+    container("node:12") {
+        resources {
+            cpu = 2.cpu
+            memory = 4.gb
+        }
+        
+        shellScript {
+            content = """
+                npm install --global gatsby-cli yarn
+                yarn install
+                gatsby telemetry --disable
+                yarn run space:build
+            """
+        }
+    }
+}
+
+job("Build .NET Guide") {
+    startOn {
+        gitPush {
+            pathFilter {
+                +"sites/dotnet-guide/**"
+                +"packages/**"
+                +"package.json"
+            }
+        }
+    }
+    
+    container("node:12") {
+        resources {
+            cpu = 2.cpu
+            memory = 4.gb
+        }
+        
+        shellScript {
+            content = """
+                npm install --global gatsby-cli yarn
+                yarn install
+                gatsby telemetry --disable
+                yarn run dotnet:build
+            """
+        }
+    }
+}
+
+job("Build IntelliJ IDEA Guide") {
+    startOn {
+        gitPush {
+            pathFilter {
+                +"sites/intellij-guide/**"
+                +"packages/**"
+                +"package.json"
+            }
+        }
+    }
+    
+    container("node:12") {
+        resources {
+            cpu = 2.cpu
+            memory = 4.gb
+        }
+        
+        shellScript {
+            content = """
+                npm install --global gatsby-cli yarn
+                yarn install
+                gatsby telemetry --disable
+                yarn run intellij:build
+            """
+        }
+    }
+}
