9b3905096078db8813f0a72a4dbf7ce08920756b
Maarten Balliauw <maarten@maartenballiauw.be>
2021-06-04 13:11:21 +0200

    Add support for html/xml/json/javascript/typescript/groovy/go/csharp/java/kotlin code examples

--- packages/gatsby-theme-bulmaio/gatsby-browser.js
+++ packages/gatsby-theme-bulmaio/gatsby-browser.js
@@ -2,8 +2,16 @@ import Prism from 'prism-react-renderer/prism'
 
 (typeof global !== "undefined" ? global : window).Prism = Prism;
 
+require("prismjs/components/prism-markup");
+require("prismjs/components/prism-json");
+require("prismjs/components/prism-typescript");
+require("prismjs/components/prism-javascript");
 require("prismjs/components/prism-kotlin");
+require("prismjs/components/prism-java");
+require("prismjs/components/prism-groovy");
+require("prismjs/components/prism-go");
 require("prismjs/components/prism-csharp");
 
-// support c#
+// Aliases
+Prism.languages["gradle"] = Prism.languages.groovy;
 Prism.languages["C#"] = Prism.languages["c#"] = Prism.languages.dotnet;
