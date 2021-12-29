a655bcaf8b94b5a05205c42c78eeb7b033861a72
Maarten Balliauw <maarten@maartenballiauw.be>
2021-12-29 08:52:06 +0100

    Bump the year to 2022

--- sites/dotnet-guide/gatsby-config.js
+++ sites/dotnet-guide/gatsby-config.js
@@ -114,7 +114,7 @@ module.exports = {
         }
       },
       footer: {
-        copyright: 'Copyright © 2000–2021 <a href="https://www.jetbrains.com/">JetBrains</a> s.r.o.'
+        copyright: 'Copyright © 2000–2022 <a href="https://www.jetbrains.com/">JetBrains</a> s.r.o.'
       }
     }
   },
