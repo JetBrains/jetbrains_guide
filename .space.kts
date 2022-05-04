4763b21c5755b55777a86720c9b3e69bbf9b43be
Maarten Balliauw <maarten@maartenballiauw.be>
2022-05-04 16:12:59 +0200

    Bump heap size

--- .space.kts
+++ .space.kts
@@ -169,27 +169,6 @@ job("Docker - Content creators image") {
     }
 }
 
-job("Warmup - Content creators (Fleet)") {
-    git {
-        depth = UNLIMITED_DEPTH
-        refSpec = "refs/*:refs/*"
-    }
-
-    startOn {
-        schedule { cron("0 0 * * *") }
-
-        gitPush {
-            enabled = true
-
-            branchFilter {
-                +"refs/heads/main"
-            }
-        }
-    }
-
-    warmup(ide = Ide.Fleet)
-}
-
 job("Warmup - Content creators (WebStorm)") {
     git {
         depth = UNLIMITED_DEPTH
