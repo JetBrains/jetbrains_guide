01951f6bd3b14afc8748f1bf5f7a318de333ada1
pauleveritt <pauleveritt@me.com>
2022-05-31 15:49:44 -0400

    Clean up the other sites/* to remove node-sass and other fossils.

--- packages/gatsby-theme-bulmaio/src/components/navbar/NavbarBrand.tsx
+++ packages/gatsby-theme-bulmaio/src/components/navbar/NavbarBrand.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-// @ts-ignore
-// import '../../../../guide-sass/NavBrand.scss';
 import NavbarBrandLogo from './NavbarBrandLogo';
 
 export interface NavbarBrandProps {
--- sites/dotnet-guide/gatsby-config.js
+++ sites/dotnet-guide/gatsby-config.js
@@ -127,7 +127,6 @@ module.exports = {
         path: `${__dirname}/contents`
       }
     },
-    'gatsby-plugin-sass',
     'gatsby-plugin-typescript',
     'gatsby-theme-bulmaio',
     {
