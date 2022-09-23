3ef41297929697812e234739d4008d7bc87fd131
pauleveritt <pauleveritt@me.com>
2022-09-23 10:55:17 -0400

    Provide some additional logging.

--- aws_video/vod_lambda.py
+++ aws_video/vod_lambda.py
@@ -24,8 +24,10 @@ def get_s3_source_key_path(event):
     this_s3 = event["Records"][0]["s3"]
     this_bucket_name = this_s3["bucket"]["name"]
     this_bucket_key = this_s3["object"]["key"]
-    print(f"#### Name: {this_bucket_name} Key: {this_bucket_key} ")
     this_bucket_path = PurePath(f"s3://{this_bucket_name}/{this_bucket_key}")
+    print(
+        f"#### Name: {this_bucket_name} .. Key: {this_bucket_key} ... Path: {this_bucket_key}"
+    )
     return this_bucket_key, this_bucket_path
 
 
@@ -42,7 +44,8 @@ def lambda_handler(event, context):
     # #### Name: jetvideo-source Key: pwe/failed_tests.mp4
     s3_key, s3_source_path = get_s3_source_key_path(event)
     s3_source_basename = s3_source_path.stem
-    print(f"####### Uploading: {s3_key}")
+    joined_parents = pathlib_parents(PurePath(s3_key))
+    print(f"####### Uploading: {s3_key} Joined Parents: {joined_parents}")
 
     region = os.environ["AWS_DEFAULT_REGION"]
     status_code = 200
@@ -64,9 +67,10 @@ def lambda_handler(event, context):
 
         # Update the job settings with the source video from the S3 event and destination
         # paths for converted videos
-        job_settings["Inputs"][0]["FileInput"] = (
-            "s3://" + "jetvideo-source" + "/" + s3_key
-        )
+        job_settings["Inputs"][0]["FileInput"] = "s3://" + str(s3_source_path)[4:]
+        # job_settings["Inputs"][0]["FileInput"] = (
+        #     "s3://" + "jetvideo-source" + "/" + s3_key
+        # )
         print(f"Job metadata", job_settings["Inputs"][0]["FileInput"])
 
         destination_s3 = "s3://" + os.environ["DestinationBucket"]
@@ -74,7 +78,8 @@ def lambda_handler(event, context):
 
         # Clean up the CDN caching
         print(f"####### CDN update")
-        cdn_path = f"/assets/pwe/{s3_source_basename}/HLS/*"
+        cdn_path = f"/assets/{joined_parents}/HLS/*"
+        print(f"##### cdn_path {cdn_path}")
         cdn_client = boto3.client("cloudfront")
         cdn_client.create_invalidation(
             DistributionId="E1166YMX8A3BF5",
@@ -85,21 +90,17 @@ def lambda_handler(event, context):
         )
 
         # HLS
-        hd = (
-            destination_s3
-            + "/"
-            + f"assets/{s3_source_basename}/HLS/{s3_source_basename}"
-        )
+        hd = destination_s3 + f"/assets/{joined_parents}/HLS/{s3_source_basename}"
         print(f"####### HLS Setup")
+        print(f"##### hd: {hd}")
         og[0]["OutputGroupSettings"]["HlsGroupSettings"]["Destination"] = hd
 
         # Thumbnails
         print(f"####### Thumbnails Setup")
         td = (
-            destination_s3
-            + "/"
-            + f"assets/{s3_source_basename}/Thumbnails/{s3_source_basename}"
+            destination_s3 + f"/assets/{joined_parents}/Thumbnails/{s3_source_basename}"
         )
+        print(f"##### td: {td}")
         og[1]["OutputGroupSettings"]["FileGroupSettings"]["Destination"] = td
 
         # Convert the video using AWS Elemental MediaConvert
@@ -111,13 +112,13 @@ def lambda_handler(event, context):
             endpoint_url=endpoints["Endpoints"][0]["Url"],
             verify=False,
         )
-        client.create_job(
-            Role=media_convert_role, UserMetadata=job_metadata, Settings=job_settings
-        )
         print(
             "##### MC settings",
             s3_source_basename,
             job_metadata,
+        )
+        client.create_job(
+            Role=media_convert_role, UserMetadata=job_metadata, Settings=job_settings
         )
         print(f"####### Finished uploading video")
 
